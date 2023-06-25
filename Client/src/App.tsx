6d61c7585c7ce337a0737b80bd2a4139d20cecfc
Kasinath S G <kasinathsg@gmail.com>
2023-06-25 08:54:07 +0400

    Created front-end for landing page

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -1,17 +1,19 @@
 import React from 'react';
 import { Route,Routes } from 'react-router-dom';
+import LandingPage from './component/LandingPage/LandingPage';
 
 
 function App() {
   return (
     <div>
       <Routes>
-          <Route element={<></>} />
+          <Route path='/'  element={<LandingPage />}/>
           <Route element={<></>} />
           <Route element={<></>} />
       </Routes>
     </div>
   );
 }
+
 
 export default App;
