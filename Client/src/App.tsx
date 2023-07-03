acc922b374aca4e0592b4d198f498651526bd82c
Kasinath S G <kasinathsg@gmail.com>
2023-07-03 21:24:10 +0400

    JSON server implementation

--- Client/src/App.tsx
+++ Client/src/App.tsx
@@ -6,6 +6,7 @@ import Dasboard from './component/Dashboard/Dasboard';
 import SimpleSidebar from './component/SideBar/LeftBar';
 import LandingPage from './component/LandingPage/LandingPage';
 import AssignTasks from './component/AssignTasks/AssignTasks';
+import PostingAnnouncements from './component/Announcements/PostingAnnouncements';
 
 
 
@@ -17,6 +18,7 @@ function App() {
           <Route path="/" element={<LandingPage />} />
           <Route path="/home" element={<Dasboard />} />
           <Route path="/assigntasks" element={<AssignTasks />} />
+          <Route path="/announcements" element={<PostingAnnouncements />} />
 
       </Routes>
     </div>
--- Client/src/component/Announcements/AnnouncementStore.tsx
+++ Client/src/component/Announcements/AnnouncementStore.tsx
@@ -1,20 +1,57 @@
-import {create} from 'zustand';
+// import {create} from 'zustand';
+
+// interface AnnouncementStore {
+//     title: string;
+//     description: string;
+//     setTitle: (title: string) => void;
+//     setDescription: (description: string) => void;
+//     resetForm: () => void;
+//   }
+  
+//   const useAnnouncementStore = create<AnnouncementStore>((set) => ({
+//     title: '',
+//     description: '',
+//     setTitle: (title) => set({ title }),
+//     setDescription: (description) => set({ description }),
+//     resetForm: () => set({ title: '', description: '' }),
+//   }));
+
+
+//   export default useAnnouncementStore;
+
+
+
+
+import { create } from 'zustand';
+
+interface Announcement {
+  id: number;
+  title: string;
+  description: string;
+  
+}
 
 interface AnnouncementStore {
-    title: string;
-    description: string;
-    setTitle: (title: string) => void;
-    setDescription: (description: string) => void;
-    resetForm: () => void;
-  }
-  
-  const useAnnouncementStore = create<AnnouncementStore>((set) => ({
+  announcements: Announcement[];
+  addAnnouncement: (announcement: Announcement) => void;
+  resetForm: () => void;
+  title: string;
+  description: string;
+  setTitle: (title: string) => void;
+  setDescription: (description: string) => void;
+}
+
+const useAnnouncementStore = create<AnnouncementStore>((set) => ({
     title: '',
     description: '',
     setTitle: (title) => set({ title }),
     setDescription: (description) => set({ description }),
-    resetForm: () => set({ title: '', description: '' }),
-  }));
+  announcements: [],
+  addAnnouncement: (announcement) =>
+    set((state) => ({
+      announcements: [...state.announcements, announcement],
+    })),
+  resetForm: () => set({ announcements: [] }),
+}));
 
-
-  export default useAnnouncementStore;
+export default useAnnouncementStore;
