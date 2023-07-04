0e3044527ba760eb96a52498d5e756a9896a700c
Kasinath S G <kasinathsg@gmail.com>
2023-07-04 10:57:34 +0400

    removing comments

--- Client/src/component/Announcements/AnnouncementStore.tsx
+++ Client/src/component/Announcements/AnnouncementStore.tsx
@@ -1,26 +1,3 @@
-// import {create} from 'zustand';
-
-// interface AnnouncementStore {
-//     title: string;
-//     description: string;
-//     setTitle: (title: string) => void;
-//     setDescription: (description: string) => void;
-//     resetForm: () => void;
-//   }
-  
-//   const useAnnouncementStore = create<AnnouncementStore>((set) => ({
-//     title: '',
-//     description: '',
-//     setTitle: (title) => set({ title }),
-//     setDescription: (description) => set({ description }),
-//     resetForm: () => set({ title: '', description: '' }),
-//   }));
-
-
-//   export default useAnnouncementStore;
-
-
-
 
 import { create } from 'zustand';
 
