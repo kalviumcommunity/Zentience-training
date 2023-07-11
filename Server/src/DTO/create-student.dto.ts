e5c400e3083cc4803bfdd497b9f63cf47021b40b
anmolsingh0001 <anmol.singh@kalvium.community>
2023-07-11 14:50:03 +0530

    add crud operation in frontend and backend

--- Server/src/DTO/create-student.dto.ts
+++ Server/src/DTO/create-student.dto.ts
@@ -2,7 +2,7 @@
 
 
 export class Createstudentdto {
-  readonly name: string;
+  readonly Name: string;
   readonly Class: string;
   readonly Rollno: string;
 }
--- Server/src/app.controller.ts
+++ Server/src/app.controller.ts
@@ -1,4 +1,12 @@
-import { Controller, Get, Body, Post } from '@nestjs/common';
+import {
+  Controller,
+  Get,
+  Body,
+  Post,
+  Patch,
+  Param,
+  Delete,
+} from '@nestjs/common';
 import { AppService } from './app.service';
 import { StudentData } from './Schemas/StudentData.schema';
 import { Createstudentdto } from './DTO/create-student.dto';
@@ -7,16 +15,34 @@ import { Createstudentdto } from './DTO/create-student.dto';
 export class AppController {
   constructor(private readonly appService: AppService) {}
 
-  @Get('/studentdata')
+  @Get('/studentData')
   async getallstudent(): Promise<StudentData[]> {
     return this.appService.findAll();
   }
 
-  @Post('/addstudent')
+  @Post('/studentData')
   async createstudent(
     @Body()
     body: Createstudentdto,
   ): Promise<StudentData> {
     return this.appService.create(body);
+  }
+
+  @Patch('/studentData/:id')
+  async updatestudent(
+    @Body()
+    body: Createstudentdto,
+    @Param('id')
+    id: number,
+  ): Promise<{ message: string }> {
+    return this.appService.update(id, body);
+  }
+
+  @Delete('/studentData/:id')
+  async deletestudent(
+    @Param('id')
+    id: number,
+  ): Promise<{ message: string }> {
+    return this.appService.delete(id);
   }
 }
